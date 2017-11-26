e96f3e332b32e119cf7c5e6b90139a04b28a6f62
apuacz <alicja.puacz@gmail.com>
2017-11-26 17:13:40 +0100

    Project 2, some mistakes by drawing in the proper order

---
+++ Proj3/src/Main.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Alicja on 2017-11-21.
+ */
+public class Main {
+}
+import processing.core.PApplet;
+
+        import java.awt.*;
+        import java.io.BufferedReader;
+        import java.io.FileReader;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
+
